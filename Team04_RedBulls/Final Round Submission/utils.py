0270ea4b41a4d30485e95c58e38fb8fa1b20c15c
Ignatius Nobel Chittilappilly <97967333+Ignatius-Nobel@users.noreply.github.com>
2024-04-13 16:06:24 +0530

    Update utils.py

--- Team04_RedBulls/Final Round Submission/utils.py
+++ Team04_RedBulls/Final Round Submission/utils.py
@@ -2,29 +2,16 @@ from sentence_transformers import SentenceTransformer
 import pinecone
 import openai
 import streamlit as st
-openai.api_key = "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"
+openai.api_key = "<OPENAI_API_KEY>"
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone.init(api_key='2c111e96-53ac-4a76-bc0f-7fe745cb2472', environment='gcp-starter')
-index = pinecone.Index('hr-bot')
+pinecone.init(api_key='<PINECONE_APIKEY>', environment='<PINECONE_ENVIRONEMENT>')  #enter pinecone api key and environment
+index = pinecone.Index('<PINECONE_INDEX_NAME>') #enter pinecone index name
 
 def find_match(input):
     input_em = model.encode(input).tolist()
     result = index.query(input_em, top_k=2, includeMetadata=True)
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
-
-# def query_refiner(conversation, query):
-
-#     response = openai.Completion.create(
-#     model="text-davinci-003",
-#     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-#     temperature=0.7,
-#     max_tokens=256,
-#     top_p=1,
-#     frequency_penalty=0,
-#     presence_penalty=0
-#     )
-#     return response['choices'][0]['text']
 
 def get_conversation_string():
     conversation_string = ""
