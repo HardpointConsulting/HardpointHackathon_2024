de3156b43565988a6ef2aa70b9565b54b1d7dc14
jibincb <85062893+jibincb@users.noreply.github.com>
2024-04-13 10:40:15 +0530

    Update check_comment1.py

--- Team01_Hacktivators/Final Round Submission/Updated codes/check_comment1.py
+++ Team01_Hacktivators/Final Round Submission/Updated codes/check_comment1.py
@@ -1,6 +1,6 @@
 from langchain.schema import (AIMessage, HumanMessage, SystemMessage)
 from langchain.chat_models import ChatOpenAI
-key = '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' #add openai secret key
+key = '' #add openai secret key
 chat = ChatOpenAI(model_name='gpt-3.5-turbo', temperature = 0.3, openai_api_key = key)
 #
 # response = chat(messages)
